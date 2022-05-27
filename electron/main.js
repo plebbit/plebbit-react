24e85c04378ceaab01bd2656349abf69f4d5a370
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-05-27 21:57:47 +0000

    only open electron links in os browser

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow, screen: electronScreen } = require('electron');
+const { app, BrowserWindow, screen: electronScreen, shell } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 const startIpfs = require('./startIpfs')
+const {URL} = require('node:url')
+
 startIpfs();
 
 const createMainWindow = () => {
@@ -37,6 +39,35 @@ const createMainWindow = () => {
       mainWindow.openDevTools()
     }
   });
+
+  // open links in external browser
+  // do not open links in plebbit-react or will lead to remote execution
+  mainWindow.webContents.on('will-navigate', (e, originalUrl) => {
+    if (originalUrl != mainWindow.webContents.getURL()) {
+      e.preventDefault()
+      try {
+        // do not let the user open any url with shell.openExternal
+        // or it will lead to remote execution https://benjamin-altpeter.de/shell-openexternal-dangers/
+
+        // only open valid https urls to prevent remote execution
+        // will throw if url isn't valid
+        const validatedUrl = new URL(originalUrl)
+        if (validatedUrl.protocol !== 'https:') {
+          throw Error(`can't open url '${originalUrl}' not https`)
+        }
+
+        // do not open http protocol, not private
+        // do not open any other protocol, will lead to remote execution
+
+        // open serialized url to prevent remote execution
+        const serializedUrl = validatedUrl.toString()
+        shell.openExternal(serializedUrl);
+      }
+      catch (e) {
+        console.warn(e)
+      }
+    }
+  })
 };
 
 app.whenReady().then(() => {
