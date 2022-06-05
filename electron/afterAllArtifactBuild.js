6d4adde818530f6803f478df86e0e4599617bcbd
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-06-05 22:49:04 +0000

    add portable to portable executable file name after windows electron build

---
+++ electron/afterAllArtifactBuild.js
@@ -0,0 +1,20 @@
+// hook that runs after electron-build
+
+const fs = require('fs-extra')
+const path = require('path')
+const distFolderPath = path.resolve(__dirname, '..', 'dist')
+
+const addPortableToPortableExecutableFileName = () => {
+  const files = fs.readdirSync(distFolderPath)
+  for (const file of files) {
+    if (file.endsWith('.exe') && !file.match('Setup')) {
+      const filePath = path.resolve(distFolderPath, file)
+      const renamedFilePath = path.resolve(distFolderPath, file.replace('.exe', ' Portable.exe'))
+      fs.moveSync(filePath, renamedFilePath)
+    }
+  }
+}
+
+exports.default = async (buildResult) => {
+  addPortableToPortableExecutableFileName()
+}
