aa4ee06e9cefabeb9b126079e710f4348bd52470
Abydin <abideenayinde7@gmail.com>
2022-06-17 12:23:22 +0100

    fixed bug conserning username

--- src/utils/getUserName.js
+++ src/utils/getUserName.js
@@ -1,17 +1,17 @@
 const getUserName = (author) => {
   let username = '';
-  if (author.displayName) {
-    username += author.displayName;
+  if (author?.displayName) {
+    username += author?.displayName;
     if (username.length > 20) {
       username = username.substring(0, 20);
     }
     username += ' ';
   }
   username += 'u/';
-  if (author.address.endsWith('.eth')) {
-    username += author.address;
+  if (author?.address.endsWith('.eth')) {
+    username += author?.address;
   } else {
-    username += author.address.substring(0, 14);
+    username += author?.address.substring(0, 14);
   }
   return username;
 };
