9869bb62b8bc137da808b5f0aed3c4a06399f096
Abydin <abideenayinde7@gmail.com>
2022-07-10 06:59:17 +0100

    fixed electron bug

--- electron/preload.js
+++ electron/preload.js
@@ -1,24 +1,35 @@
 // force IpfsHttpClient to use node-fetch to fix max 6 connections
 // very hacky, might break at some point, wont be needed after
 // switch to contextIsolation
-const mock = require('mock-require')
-const fetch = require('node-fetch')
+const mock = require('mock-require');
+const fetch = require('node-fetch');
 mock('ipfs-utils/src/env.js', {
-  isTest: false, isElectron: false, isElectronMain: false, 
-  isElectronRenderer: false, isNode: true, isBrowser: false, 
-  isWebWorker: false, isEnvWithDom: false, isReactNative: false
-})
-mock('native-fetch', {default: fetch.default, Headers: fetch.Headers, Request: fetch.Request, Response: fetch.Response})
+  isTest: false,
+  isElectron: false,
+  isElectronMain: false,
+  isElectronRenderer: false,
+  isNode: true,
+  isBrowser: false,
+  isWebWorker: false,
+  isEnvWithDom: false,
+  isReactNative: false,
+});
+mock('native-fetch', {
+  default: fetch.default,
+  Headers: fetch.Headers,
+  Request: fetch.Request,
+  Response: fetch.Response,
+});
 
 // dev uses http://localhost, prod uses file://...index.html
-const isDev = window.location.protocol === 'http:'
+const isDev = window.location.protocol === 'http:';
 
 // add PlebbitJs to window.PlebbitJs so that the hooks
 // can call setPlebbitJs(PlebbitJs) and use Plebbit with
 // all node privileges
 const PlebbitJs = require('@plebbit/plebbit-js');
-const envPaths = require('env-paths').default('plebbit', {suffix: false})
-window.PlebbitJs = PlebbitJs
+const envPaths = require('env-paths').default('plebbit', { suffix: false });
+window.PlebbitJs = PlebbitJs;
 window.DefaultPlebbitOptions = {
   // find the user's OS data path
   dataPath: !isDev ? envPaths.data : undefined,
@@ -29,9 +40,13 @@ window.DefaultPlebbitOptions = {
   ipfsGatewayUrl: 'http://localhost:11028',
   // ipfsGatewayUrl: 'https://cloudflare-ipfs.com',
   // pubsubHttpClientOptions: 'https://pubsubprovider.xyz/api/v0',
-}
+};
 
 // TODO: create context isolated plebbit-js functions to use
 // without contextIsolation: true for improved security
 
-localStorage.debug = 'plebbit-js:*,plebbit-react-hooks:*'
+try {
+  localStorage.debug = 'plebbit-js:*,plebbit-react-hooks:*';
+} catch (e) {
+  console.log(e);
+}
