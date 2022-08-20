f0a52fc185dd37181e5ada21948d62325c0eb3c9
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-08-20 18:43:51 +0000

    fix sidebar links

--- src/components/sidebar/bottomSideBar.jsx
+++ src/components/sidebar/bottomSideBar.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import plebbitReactPackageJson from '../../../package.json';
-import { Link } from 'react-router-dom';
 
 const BottomSideBar = ({ bg }) => {
   const Bg = useColorModeValue('lightBody', 'darkBody');
   const bottomData1 = [
-    { label: 'Help', link: '' },
-    { label: 'Plebbit Coins', link: '' },
-    { label: 'Plebbit Premium', link: '' },
-    { label: 'Communities', link: '' },
-    { label: 'Plebbit', link: '' },
-    { label: 'Topics', link: '' },
+    { label: 'Report a Bug', link: 'https://t.me/plebbit' },
+    { label: 'PLEB Token', link: 'https://plebbit.eth.limo' },
+    { label: 'PLEB NFTs', link: 'https://plebbit.eth.limo' },
+    { label: 'Win/Mac/Linux/Android Client', link: 'https://github.com/plebbit/plebbit-react/releases/latest' },
+    { label: 'Whitepaper', link: 'https://github.com/plebbit/whitepaper/discussions/2' },
+    { label: 'Github', link: 'https://github.com/plebbit' },
+    { label: 'Open Source Code', link: 'https://github.com/plebbit/plebbit-js/blob/master/LICENSE' },
   ];
   const bottomData2 = [
-    { label: 'About', link: '' },
-    { label: 'Careers', link: '' },
-    { label: 'Press', link: '' },
-    { label: 'Advertise', link: '' },
-    { label: 'Blog', link: '' },
-    { label: 'Content Policy', link: '' },
-    { label: 'Privacy Policy', link: '' },
-    { label: ' Mod Policy', link: '' },
+    { label: 'About', link: 'https://medium.com/@plebbit/about-plebbit-3edb35ac48f1' },
+    { label: 'We Are Hiring JS Devs', link: 'https://medium.com/@plebbit/plebbit-is-hiring-javascript-developers-cc20980b42ae' },
+    { label: 'Twitter', link: 'https://twitter.com/getplebbit' },
+    { label: 'Telegram', link: 'https://t.me/plebbit' },
+    { label: 'Blog', link: 'https://medium.com/@plebbit' },
+    { label: 'Discord', link: 'https://discord.gg/E7ejphwzGW' },
+    { label: 'Matrix', link: 'https://matrix.to/#/#plebbit:plebbitchat.org' },
   ];
 
   return (
@@ -31,7 +30,7 @@ const BottomSideBar = ({ bg }) => {
           <Flex padding="12px 8px">
             <Flex flexFlow="column" flexWrap="nowrap" width="50%" padding="0 4px">
               {bottomData1?.map((item, index) => (
-                <Link to={item?.link} key={index}>
+                <a target="_blank" rel="noreferrer" href={item?.link} key={index}>
                   <Box
                     fontSize="12px"
                     fontWeight="400"
@@ -43,12 +42,12 @@ const BottomSideBar = ({ bg }) => {
                   >
                     {item?.label}
                   </Box>
-                </Link>
+                </a>
               ))}
             </Flex>
             <Flex flexFlow="column" flexWrap="nowrap" width="50%" padding="0 4px">
               {bottomData2?.map((item, index) => (
-                <Link to={item?.link} key={index}>
+                <a target="_blank" rel="noreferrer" href={item?.link} key={index}>
                   <Box
                     fontSize="12px"
                     fontWeight="400"
@@ -60,7 +59,7 @@ const BottomSideBar = ({ bg }) => {
                   >
                     {item?.label}
                   </Box>
-                </Link>
+                </a>
               ))}
             </Flex>
           </Flex>
@@ -73,7 +72,7 @@ const BottomSideBar = ({ bg }) => {
             marginBottom="3px"
             display="inline-block"
           >
-            plebbit v{plebbitReactPackageJson.version} GPL-2.0
+            Plebbit v{plebbitReactPackageJson.version} GPL-2.0
           </Flex>
         </Box>
       </Box>
