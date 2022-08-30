67205e0b85e27db55861b3304a60a93dd5b73374
Abydin <abideenayinde7@gmail.com>
2022-08-30 06:29:50 +0100

    removed markdown  parsing of content

--- src/components/Editor/marked.jsx
+++ src/components/Editor/marked.jsx
@@ -1,17 +1,17 @@
-var MarkdownIt = require('markdown-it');
-import parse from 'html-react-parser';
+// var MarkdownIt = require('markdown-it');
+// import parse from 'html-react-parser';
 
 const Marked = ({ content }) => {
-  MarkdownIt;
-  var md = new MarkdownIt({
-    html: true,
-    xhtmlOut: true,
-    breaks: true,
-    linkify: true,
-    typographer: true,
-  });
+  // MarkdownIt;
+  // var md = new MarkdownIt({
+  //   html: true,
+  //   xhtmlOut: true,
+  //   breaks: true,
+  //   linkify: true,
+  //   typographer: true,
+  // });
 
-  return parse(md.render(content));
+  return content;
 };
 
 export default Marked;
