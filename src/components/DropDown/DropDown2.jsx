ebc982ef9706cfde338552a8e164b328ac239680
Abydin <36446984+Abydin@users.noreply.github.com>
2022-10-18 20:32:01 +0100

    Revert "made some fixes for home left bar"

--- src/components/DropDown/DropDown2.jsx
+++ src/components/DropDown/DropDown2.jsx
@@ -38,6 +38,8 @@ const DropDown2 = ({
   const Control = ({ children, ...props }) => {
     const style = { cursor: 'pointer' };
 
+    console.log(props?.selectProps);
+
     return (
       <components.Control {...props}>
         {(prefix && prefix(props?.selectProps?.value)) || (
