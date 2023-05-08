472d9011b292321a429887983f17e12bb3ebab0b
Abydin <abideenayinde7@gmail.com>
2023-05-08 11:21:49 +0100

    fixed removed post still showing media

--- src/utils/onError.js
+++ src/utils/onError.js
@@ -1,7 +1,8 @@
 import { toast } from 'react-toastify';
+import truncateString from './truncateString';
 
 const onError = (error) => {
-  return toast.error(error?.toString(), {
+  return toast.error(truncateString(error?.toString(), 500), {
     position: 'bottom-center',
     autoClose: false,
     hideProgressBar: false,
