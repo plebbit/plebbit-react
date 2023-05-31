f2c15cc4640c643f58f034eda991be81b3f81904
Abydin <abideenayinde7@gmail.com>
2023-05-31 12:52:47 +0100

    change statestring location to loader on feeds

--- src/components/Editor/marked.jsx
+++ src/components/Editor/marked.jsx
@@ -1,11 +1,30 @@
 var MarkdownIt = require('markdown-it');
+
+// import ReactMarkdown from "react-markdown";
+
+
 import parse from 'html-react-parser';
 import { sanitize } from 'dompurify';
 
+
 const Marked = ({ content }) => {
   MarkdownIt;
-  var md = new MarkdownIt({});
-  return parse(sanitize(md.render(content)));
+  var md = new MarkdownIt('commonmark', {
+    breaks: true,
+    html: true,
+    xhtmlOut: true,
+    typographer: true,
+
+
+  });
+  return (parse(sanitize(md.render(content))));
 };
 
 export default Marked;
+
+
+
+
+
+
+
--- src/components/InfiniteScroll/index.jsx
+++ src/components/InfiniteScroll/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Infs from 'react-infinite-scroll-component';
 import { Virtuoso } from 'react-virtuoso'
-import logger from '../../utils/logger';
 
 const InfiniteScroll = ({ feeds, loader, hasMore, loadMore, content }) => {
 
@@ -9,33 +7,13 @@ const InfiniteScroll = ({ feeds, loader, hasMore, loadMore, content }) => {
 
   let Loading
   if (hasMore) {
-    Loading = () => loader
+    Loading = () =>
+      loader
   }
   return (
-    // <Infs
-    //   dataLength={feeds ? feeds.length : 0}
-    //   next={() => {
-    //     try {
-    //       loadMore();
-    //     } catch (error) {
-    //       logger('load-more', error, 'trace');
-    //     }
-    //   }}
-    //   hasMore={hasMore}
-    //   loader={loader}
-    //   // below props only if you need pull down functionality
-    //   refreshFunction={() => {}}
-    //   // pullDownToRefresh
-    //   pullDownToRefreshThreshold={50}
-    //   pullDownToRefreshContent={
-    //     <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-    //   }
-    //   releaseToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>}
-    // >
-    //   {feeds?.map((feed) => content(feed))}
-    // </Infs>
+
     <Virtuoso
-      increaseViewportBy={{bottom: 600, top: 200}}
+      increaseViewportBy={ { bottom: 600, top: 200 } }
       totalCount={ feeds ? feeds.length : 0 }
       data={ feeds }
       style={ { maxWidth: '100%' } }
