77d6befc546474055ac65b2a2a8e41731482622e
Abydin <abideenayinde7@gmail.com>
2023-08-07 15:19:49 +0100

    done with  text post image on classic post

--- src/components/Post/ClassicPost2.jsx
+++ src/components/Post/ClassicPost2.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import styles from './classic-post.module.css'
+import PostVote from './PostVote'
+import { BsFileText } from 'react-icons/bs'
 
-const ClassicPost2 = () => {
+const ClassicPost2 = ({
+    loading,
+    vote,
+    post,
+    type,
+    showContent,
+    setShowContent,
+    location,
+    copied,
+    detail,
+    isOnline,
+    handleOption,
+    subPlebbit: sub,
+    postVotes,
+    handleCopy,
+    pending,
+    detailRoute,
+    openRemovalModal,
+    allowedSpecial,
+    owner,
+    showSpoiler,
+    setShowSpoiler,
+    mediaInfo,
+    hasThumbnail,
+    commentCount,
+    upVote,
+    downVote,
+    editLabel,
+    authorPath,
+    stateString,
+    blocked, muted
+}) => {
     return (
-        <div>ClassicPost2</div>
+        <div className={ styles.wrapper }>
+            {/* Vote Bar */ }
+            <PostVote vote={ vote } upVote={ upVote } downVote={ downVote } postVotes={ postVotes } />
+            <div className={ styles.main }>
+                <div className={ styles.main_wrap }>
+                    <div className={ styles.left }>
+                        <div className={ styles.left_wrap }>
+                            <div className={ styles.icon_wrap }>
+                                <BsFileText className={ `${styles.icon} ${styles.icon_text_post}` } />
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className={ styles.right }></div>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
