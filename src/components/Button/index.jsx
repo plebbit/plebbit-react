a3753ebcb3786a1db476e3aeb81d6a71a09e2bff
Abydin <abideenayinde7@gmail.com>
2023-10-18 11:18:41 +0100

    converted editor

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,73 +2,6 @@ import React from 'react';
 import { Button as Btn } from '@chakra-ui/button';
 import styles from './button.module.css';
 
-const Button = ({ children, ...props }) => {
-  const {
-    onClick,
-    content,
-    loading,
-    padding,
-    lineHeight,
-    fontSize,
-    display,
-    bg,
-    border,
-    color,
-    disabled,
-    width,
-    height,
-    margin,
-    position,
-    fontWeight,
-    minH,
-    minW,
-    borderRadius,
-    sx,
-    _hover,
-    fill,
-    backgroundColor,
-    cursor,
-  } = props;
-  return (
-    <Btn
-      onClick={onClick}
-      isLoading={loading}
-      _hover={
-        _hover || {
-          opacity: '0.8',
-        }
-      }
-      backgroundColor={backgroundColor}
-      display={display}
-      borderColor={border === 'none' ? '' : border}
-      borderRadius={borderRadius || '9999px'}
-      bg={bg}
-      border={border === 'none' ? 'none' : '1px solid'}
-      h={height}
-      minH={minH}
-      minW={minW || '32px'}
-      width={width || 'auto'}
-      cursor={cursor || 'pointer'}
-      fontFamily="Noto Sans, Arial, sans-serif"
-      fontWeight={fontWeight || '700'}
-      lineHeight={lineHeight || '17px'}
-      padding={padding || '4px 16px'}
-      fontSize={fontSize || '14px'}
-      color={color}
-      disabled={disabled}
-      margin={margin}
-      position={position}
-      sx={sx}
-      fill={fill}
-      {...props}
-    >
-      {content || children}
-    </Btn>
-  );
-};
-
-export default Button;
-
 export const Button2 = ({ children, className, disabled, onClick, ...rest }) => {
   return (
     <button
@@ -82,3 +15,5 @@ export const Button2 = ({ children, className, disabled, onClick, ...rest }) => 
     </button>
   );
 };
+
+export default Button2;
--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,159 +1,45 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import { convertToRaw } from 'draft-js';
-import { Box, Flex, Textarea, useColorModeValue } from '@chakra-ui/react';
-import { Editor as MarkdownEditor } from 'react-draft-wysiwyg';
-import draftToMarkdown from 'draftjs-to-markdown';
+import React from 'react';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Wrapper from './style';
+import styles from './editor.module.css';
 import Button from '../Button';
 
 const Editor = ({
-  hideToolBar,
-  wrapperClassName,
-  editorClassName,
-  toolbarClassName,
+  value,
+  placeholder,
   setValue,
-  setEditorState,
-  editorState,
   handleSubmit,
   showSubmit,
   submitBtnStyle,
   submitBtnText,
   otherBtn,
-  loading,
-  placeholder,
-  value
 }) => {
-  const toolbarBg = useColorModeValue('#F6F7F8', '#272729');
-  const toolbarColor = useColorModeValue('#818384', '#818384');
-  const wrapperBorder = useColorModeValue('#edeff1', '#343536');
-  const [disabled, setDisabled] = useState(true);
-
-  const handleChange = (data) => {
-
-    if (draftToMarkdown(convertToRaw(data.getCurrentContent())).length) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-    setEditorState(data);
-    setValue(() => draftToMarkdown(convertToRaw(data.getCurrentContent())));
-  };
-
-
   return (
-    <Wrapper toolbarBg={ toolbarBg } toolbarColor={ toolbarColor } wrapperBorder={ wrapperBorder }>
-      <Box className='wrapperClassName'>
-        <Textarea
-          placeholder="Text"
-          onChange={ (e) => setValue(e.target.value) }
-          value={ value }
-          className='editorClassName'
-          outline='none'
-        />
-      </Box>
-
-      { 1 == 2 && <MarkdownEditor
-        toolbarClassName={ toolbarClassName || 'toolbarClassName' }
-        wrapperClassName={ wrapperClassName || 'wrapperClassName' }
-        editorClassName={ editorClassName || 'editorClassName' }
-        onEditorStateChange={ handleChange }
-        editorState={ editorState }
-        toolbarHidden={ hideToolBar }
-        placeholder={ placeholder }
-        toolbar={ {
-          options: [
-            'inline',
-            'blockType',
-            'fontSize',
-            'fontFamily',
-            'list',
-            'textAlign',
-            'colorPicker',
-            'link',
-            'embedded',
-            'emoji',
-            'image',
-            'remove',
-            'history',
-          ],
-          inline: {
-            bold: { className: 'button-class' },
-            italic: { className: 'button-class' },
-            underline: { className: 'button-class' },
-            strikethrough: { className: 'button-class' },
-            monospace: { className: 'button-class' },
-            superscript: { className: 'button-class' },
-            subscript: { className: 'button-class' },
-          },
-          blockType: {
-            className: 'button-class',
-            dropdownClassName: 'demo-dropdown-custom',
-          },
-          fontSize: { className: 'button-class' },
-          list: {
-            unordered: { className: 'button-class' },
-            ordered: { className: 'button-class' },
-            indent: { className: 'button-class' },
-            outdent: { className: 'button-class' },
-          },
-          textAlign: {
-            left: { className: 'button-class' },
-            center: { className: 'button-class' },
-            right: { className: 'button-class' },
-            justify: { className: 'button-class' },
-          },
-          fontFamily: {
-            className: 'button-class',
-            dropdownClassName: 'demo-dropdown-custom',
-          },
-          colorPicker: { className: 'button-class', popupClassName: 'demo-popup-custom' },
-          link: {
-            popupClassName: 'button-class',
-            link: { className: 'button-class' },
-            unlink: { className: 'button-class' },
-          },
-          emoji: { className: 'button-class', popupClassName: 'demo-popup-custom' },
-          embedded: { className: 'button-class', popupClassName: 'demo-popup-custom' },
-          image: {
-            className: 'button-class',
-            popupClassName: 'demo-popup-custom',
-          },
-          remove: { className: 'button-class' },
-          history: {
-            undo: { className: 'button-class' },
-            redo: { className: 'button-class' },
-          },
-        } }
-      /> }
-      { showSubmit && (
-        <Flex
-          width="100%"
-          bg={ toolbarBg }
-          borderRadius="0 0 4px 4px"
-          opacity={ 1 }
-          mb="25px"
-          zIndex="1"
-          alignItems="center"
-        >
-          { otherBtn }
+    <div className={styles.wrapper}>
+      <textarea
+        className={styles.editor}
+        placeholder={placeholder || 'What are your thoughts?'}
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      {showSubmit && (
+        <div className={styles.editor_footer}>
           <Button
-            content={ submitBtnText || 'comment' }
-            cursor="pointer"
-            disabled={ !value?.length }
-            padding="4px 8px"
-            minH="24px"
-            margin="4px 8px"
-            height=""
-            marginLeft="auto"
-            sx={ { filter: 'grayscale(1)', ...submitBtnStyle } }
-            onClick={ handleSubmit }
-            loading={ loading }
-          />
-        </Flex>
-      ) }
-    </Wrapper>
+            style={{
+              color: '#fff',
+              backgroundColor: '#0079d3',
+              ...submitBtnStyle,
+            }}
+            onClick={handleSubmit}
+            disabled={!value?.length}
+          >
+            {submitBtnText || 'Comment'}
+          </Button>
+
+          {otherBtn}
+        </div>
+      )}
+    </div>
   );
 };
 
