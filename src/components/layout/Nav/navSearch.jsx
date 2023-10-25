7b41accf9f1f9085bd76a2ca2e642d6618ebcb9e
Abydin <abideenayinde7@gmail.com>
2023-10-25 09:06:46 +0100

    fixed searchh for subs

--- src/components/layout/Nav/navSearch.jsx
+++ src/components/layout/Nav/navSearch.jsx
@@ -1,15 +1,84 @@
-import { useSubplebbit } from '@plebbit/plebbit-react-hooks';
-import React, { useState } from 'react';
+import {
+  useAccount,
+  useAccountSubplebbits,
+  useSubplebbit,
+  useSubplebbits,
+} from '@plebbit/plebbit-react-hooks';
+import React, { useMemo, useState } from 'react';
 import { RiSearchLine } from 'react-icons/ri';
 import getIsOnline from '../../../utils/getIsOnline';
 import Avatar from '../../Avatar';
 import styles from './navbar.module.css';
 import { Link } from 'react-router-dom';
+import useStore from '../../../store/useStore';
+import truncateString from '../../../utils/truncateString';
+import Sort from '../../../utils/sort';
+import convertArrToObj from '../../../utils/convertArrToObj';
+import { getSubName } from '../../../utils/getUserName';
 
-export const NavSearch = () => {
+export const NavSearch = React.memo(() => {
   const [searchVal, setSearchVal] = useState('');
+  const profile = useAccount();
+  const { accountSubplebbits } = useAccountSubplebbits();
+  const subscriptions = useSubplebbits({ subplebbitAddresses: profile?.subscriptions });
+  const { subPlebbitData, subPlebbitDefData } = useStore((state) => state);
+  const mySubplebbits = Object.keys(accountSubplebbits)?.length
+    ? Object.keys(accountSubplebbits)?.map((pages) => ({
+        label: truncateString(accountSubplebbits[pages]?.title),
+        value: pages,
+        ...accountSubplebbits[pages],
+      }))
+    : [];
+  const subs = subscriptions?.length
+    ? subscriptions?.map((x) => ({ ...x, value: x?.address, label: x?.title }))
+    : '';
 
   const data = useSubplebbit(searchVal ? { subplebbitAddress: searchVal } : undefined);
+  const options = useMemo(
+    () =>
+      Sort(
+        convertArrToObj(
+          [
+            ...mySubplebbits,
+            ...subs,
+            [
+              subPlebbitData.map((x) => ({
+                ...x,
+                label: getSubName(x),
+                value: x?.address,
+              })),
+              subPlebbitDefData
+                ?.filter((x) => x !== undefined)
+                .map((x) => ({
+                  ...x,
+                  label: getSubName(x),
+                  value: x?.address,
+                })),
+            ].flat(),
+          ]
+            ?.filter((x) => x !== undefined)
+            .flat(),
+          'value',
+          true
+        ),
+        (x) => getIsOnline(x?.updatedAt),
+        true
+      ),
+    [mySubplebbits, subs, subPlebbitDefData]
+  );
+
+  const searchResult = useMemo(
+    () =>
+      options?.filter(
+        (x) =>
+          x?.address.includes(searchVal?.replace(/\s/g, '')?.toLowerCase()) ||
+          x?.title
+            ?.replace(/\s/g, '')
+            ?.toLowerCase()
+            ?.includes(searchVal?.replace(/\s/g, '')?.toLowerCase())
+      ),
+    [searchVal]
+  );
 
   return (
     <div className={styles.ns_wrap}>
@@ -21,35 +90,64 @@ export const NavSearch = () => {
             </div>
           </label>
           <input
+            list="subs"
             type="search"
             placeholder="Search subplebbit"
             value={searchVal}
             onChange={(e) => setSearchVal(e.target.value)}
+            autoComplete="off"
           />
         </form>
       </div>
       {searchVal && (
-        <div className={styles.ns_result_wrap}>
-          <Link to={`/p/${searchVal}/`} className={styles.ns_result}>
-            <Avatar
-              avatar={data?.avatar}
-              width={24}
-              height={24}
-              style={{
-                marginRight: '8px',
-              }}
-              badge={data !== undefined ? true : false}
-              isOnline={getIsOnline(data?.updateAt)}
-            />
-            <div className={styles.ns_result2}>
-              <div className={styles.nsr_title}>{data?.title}</div>
-              <div className={styles.nsr_add}> {searchVal}</div>
-            </div>
-          </Link>
+        <div id="subs" className={styles.ns_result_wrap}>
+          {searchResult?.length ? (
+            searchResult?.map((val, index) => (
+              <Link
+                to={`/p/${searchVal}/`}
+                value={val?.address}
+                name={val?.title}
+                key={index}
+                className={styles.ns_result}
+              >
+                <Avatar
+                  avatar={val?.suggested?.avatarUrl}
+                  width={24}
+                  height={24}
+                  style={{
+                    marginRight: '8px',
+                  }}
+                  badge
+                  isOnline={getIsOnline(data?.updateAt)}
+                />
+                <div className={styles.ns_result2}>
+                  <div className={styles.nsr_title}>{val?.title}</div>
+                  <div className={styles.nsr_add}> {val?.address}</div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <Link to={`/p/${searchVal}/`} className={styles.ns_result}>
+              <Avatar
+                avatar={data?.suggested?.avatarUrl}
+                width={24}
+                height={24}
+                style={{
+                  marginRight: '8px',
+                }}
+                badge={data !== undefined ? true : false}
+                isOnline={getIsOnline(data?.updateAt)}
+              />
+              <div className={styles.ns_result2}>
+                <div className={styles.nsr_title}>{data?.title}</div>
+                <div className={styles.nsr_add}> {searchVal}</div>
+              </div>
+            </Link>
+          )}
         </div>
       )}
     </div>
   );
-};
+});
 
 export default NavSearch;
--- src/hooks/useSubPlebbitDefaultData.js
+++ src/hooks/useSubPlebbitDefaultData.js
@@ -6,13 +6,14 @@ const useSubPlebbitDefaultData = () => {
   const subplebbits = async () => {
     try {
       const response = await fetch(
-        'https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/subplebbits.json',
+        'https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json',
         { cache: 'no-cache' }
       );
 
       if (response.ok) {
         const data = await response.json();
-        setValue(data);
+        console.log({ data });
+        setValue(data?.subplebbits);
       } else {
         toast.error('error fetching default subs', {
           position: 'bottom-center',
@@ -49,47 +50,96 @@ const useSubPlebbitDefaultData = () => {
         },
         {
           address: 'meta.plebchan.eth',
+          tags: ['plebchan', 'web3'],
+          features: ['sfw'],
         },
         {
           title: 'Plebbit Token',
           address: 'plebtoken.eth',
+          tags: ['ethereum', 'eth', 'crypto'],
+          features: ['sfw'],
         },
         {
           title: 'Plebbit Lore',
           address: 'pleblore.eth',
+          tags: ['storytelling'],
+          features: ['sfw'],
         },
         {
           title: 'Brasilandia',
           address: 'brasilandia.eth',
+          tags: ['brazil'],
+          features: ['sfw'],
         },
         {
           title: '/pol/',
           address: 'politically-incorrect.eth',
+          tags: ['politics', 'news'],
         },
         {
           title: '/biz/',
           address: 'business-and-finance.eth',
+          tags: ['crypto', 'defi', 'finance', 'business'],
         },
         {
           address: 'movies-tv-anime.eth',
+          tags: ['movies', 'tv', 'anime'],
+          features: ['sfw'],
         },
         {
           address: 'plebmusic.eth',
+          tags: ['music'],
+          features: ['sfw'],
         },
         {
           address: 'videos-livestreams-podcasts.eth',
+          tags: ['podcast', 'video', 'livestream'],
         },
         {
           address: 'health-nutrition-science.eth',
+          tags: ['health', 'fitness'],
+          features: ['sfw'],
         },
         {
           address: 'censorship-watch.eth',
+          tags: ['censorship', 'freespeech'],
         },
         {
           address: 'reddit-screenshots.eth',
+          tags: ['reddit', 'funny'],
         },
         {
           address: 'plebbit-italy.eth',
+          tags: ['italy'],
+          features: ['sfw'],
+        },
+        {
+          address: 'weaponized-autism.eth',
+          tags: ['motivation', 'autism'],
+        },
+        {
+          address: 'monarkia.eth',
+        },
+        {
+          address: 'mktwallet.eth',
+          tags: ['marketing', 'business'],
+        },
+        {
+          address: 'pleblabs.eth',
+        },
+        {
+          address: '💩posting.eth',
+          tags: ['funny', 'meme'],
+        },
+        {
+          address: 'bitcoinbrothers.eth',
+        },
+        {
+          address: 'plebbrothers.eth',
+        },
+        {
+          address: 'networkeconomics.eth',
+          tags: ['networking'],
         },
         {
           title: 'Thrifty Plebs',
@@ -106,6 +156,19 @@ const useSubPlebbitDefaultData = () => {
         {
           title: 'Anti Plebbit',
           address: 'antiplebbit.eth',
+        },
+        {
+          title: 'Ask Plebbit',
+          address: 'askplebbit.eth',
+        },
+        {
+          title: 'Current Events',
+          address: 'currentevents.eth',
+          tags: ['politics', 'news'],
+        },
+        {
+          title: 'So I Became Comfy',
+          address: 'soibecamecomfy.eth',
         },
         {
           title: 'Server of indecision',
